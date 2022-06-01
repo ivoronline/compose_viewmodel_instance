32324164ba39fed2c13e55251cd11583600784fc
igoreta <ivoronline@gmail.com>
2022-06-01 13:40:31 +0200

    Initial commit

---
+++ app/src/main/java/com/ivoronline/compose_viewmodel_instance/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ivoronline.compose_viewmodel_instance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+
+//==========================================================
+// MAIN ACTIVITY
+//==========================================================
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val myViewModel = MyViewModel()       //Create Model Instance
+
+    setContent {
+      MyCounter(myViewModel)              //Model Instance is the only Parameter
+    }
+
+  }
+}
+
+//==========================================================
+// COUNTER (Destination Composable)
+//==========================================================
+@Composable
+fun MyCounter(myViewModel: MyViewModel) {
+  Button({
+    myViewModel.increase(10)                                             //Call Model Functions
+    myViewModel.concat  (" hi")                                          //Call Model Functions
+  }) {
+    Text("(Count, Name) = (${myViewModel.count}, ${myViewModel.name})")  //Display State Variables
+  }
+}
+
